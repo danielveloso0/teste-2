de98d53046a6fec699e489ccdc7420639054957a
Daniel Veloso <daniel.f.veloso@ufv.br>
2024-02-17 15:22:25 -0300

    update script2

--- python/script2.py
+++ python/script2.py
@@ -1,1 +1,8 @@
+import pandas as pd 
 
+tabela = {'país':["Brasil","Rússia",'Argentina','Finlândia'],'valor':[50,150,25,100]}
+df = pd.DataFrame(tabela)
+print(df.head())
+import matplotlib.pyplot as plt
+plt.bar(x=df['país'],height=df['valor'])
+plt.show()
